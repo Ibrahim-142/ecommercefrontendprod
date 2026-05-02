727013bcdf529fe27e06ba024841c49f4ac7855a
Ibrahim-142 <ibrahimkhurshid65@gmail.com>
2026-05-02 18:19:01 +0500

    this local frontend is working with deployed backend

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,46 +1,47 @@
 import { useState, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
 import { useNavigate } from "react-router";
-import API from "../../api/auth"; // your axios instance
+import api from "../../api/axios";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true); // start as true
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   // Check auth on mount
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const res = await API.get("/auth/me", { withCredentials: true });
+        const res = await api.get("/api/auth/me");
         setUser(res.data);
       } catch (err) {
         if (err.response?.status !== 401) {
-          console.error(err); // only log real issues
+          console.error(err);
         }
         setUser(null);
       } finally {
         setLoading(false);
       }
     };
+
     checkAuth();
   }, []);
+
   const register = async (data) => {
     setLoading(true);
     try {
-      const res = await API.post("/auth/register", data, {
-        withCredentials: true,
-      });
+      const res = await api.post("/api/auth/register", data);
       setUser(res.data.user);
       navigate("/");
     } finally {
       setLoading(false);
     }
   };
+
   const login = async (data) => {
     setLoading(true);
     try {
-      const res = await API.post("/auth/login", data, { withCredentials: true });
+      const res = await api.post("/api/auth/login", data);
       setUser(res.data.user);
       navigate("/homepage");
     } finally {
@@ -51,7 +52,7 @@ export const AuthProvider = ({ children }) => {
   const logout = async () => {
     setLoading(true);
     try {
-      await API.post("/auth/logout", {}, { withCredentials: true });
+      await api.post("/api/auth/logout");
       setUser(null);
       navigate("/");
     } finally {
